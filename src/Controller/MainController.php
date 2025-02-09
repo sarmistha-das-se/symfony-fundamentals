fdd5ab82b88b83653858a9471de563ac973edbb4
Sarmistha Das <sarmistha.das.se@gmail.com>
2025-02-09 19:30:56 +0530

    Twig templates

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -2,14 +2,25 @@
 
 namespace App\Controller;
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
-class MainController
+class MainController extends AbstractController
 {
   #[Route('/')]
   public function homepage()
   {
-    return new Response('<strong> Starshop</strong>');
+
+    $starshipCount = 457;
+    $myShip = [
+      'name' => 'Star Shop Name',
+      'class' => 'Garden',
+      'status' => 'In Progress',
+    ];
+    return $this->render('main/homepage.html.twig', [
+      'numberOfStarShips' => $starshipCount,
+      'myship' => $myShip
+    ]);
   }
 }
